3c0971de3ed08ab3b55d34d4bed28fbe75148956
Ayush Singh <you@example.com>
2026-03-24 12:53:35 +0530

    Fix: Clean build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
-**Ctrl + S** save karo.
-
----
-
-Ab `runtime.txt` bhi banao — same tarah root mein right click → New File → `runtime.txt` → yeh likho:
-```
-python-3.11.0
